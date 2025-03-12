f066515bf426d960a69a0f688d8c00b57c8556c4
Cyndie Russeeawon <russeeawon@hall.ru.ac.za>
2025-03-12 10:00:25 +0200

    Merge conflicts for bump 0.2.5

--- quartical/gains/__init__.py
+++ quartical/gains/__init__.py
@@ -9,10 +9,7 @@ from quartical.gains.rotation_measure import RotationMeasure
 from quartical.gains.crosshand_phase import CrosshandPhase, CrosshandPhaseNullV
 from quartical.gains.leakage import Leakage
 from quartical.gains.delay_and_tec import DelayAndTec
-<<<<<<< HEAD
-=======
 from quartical.gains.parallactic_angle import ParallacticAngle
->>>>>>> upstream/main
 
 
 TERM_TYPES = {
@@ -26,13 +23,8 @@ TERM_TYPES = {
     "rotation": Rotation,
     "rotation_measure": RotationMeasure,
     "crosshand_phase": CrosshandPhase,
-<<<<<<< HEAD
-    "leakage": Leakage,
-    "delay_and_tec": DelayAndTec
-=======
     "crosshand_phase_null_v": CrosshandPhaseNullV,
     "leakage": Leakage,
     "delay_and_tec": DelayAndTec,
     "parallactic_angle": ParallacticAngle
->>>>>>> upstream/main
 }
