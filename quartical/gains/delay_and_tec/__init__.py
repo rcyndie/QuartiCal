893234443fe89f34d2c4acc37b478cbbb3a35d08
Cyndie Russeeawon <russeeawon@hall.ru.ac.za>
2025-08-22 23:03:13 +0200

    Added mediam_filter code

--- quartical/gains/delay_and_tec/__init__.py
+++ quartical/gains/delay_and_tec/__init__.py
@@ -1,6 +1,5 @@
 import numpy as np
 import finufft
-from scipy.signal import medfilt
 from scipy.ndimage import median_filter
 from collections import namedtuple
 from quartical.gains.conversion import no_op, trig_to_angle
@@ -302,7 +301,23 @@ class DelayAndTec(ParameterizedGain):
                     params[:, :, p, 0, 2] = tec_arr[:, p, 1][:, np.newaxis]
 
 
+        #Choose a window size that must be odd.
+        window_size = 30
+        # run_median_filter = False
+        run_median_filter = True
+
+
+        if run_median_filter:
+            for p in range(n_ant):
+                #Check for param_flags.
+                flag_ind = param_flags[:, 0, p, 0] == 1
+                for par in range(params.shape[-1]):
+                    par_copy = params[:, 0, p, 0, par][~flag_ind].copy()
+
+                    #Run median_filter on only unflagged entries.
+                    params[:, 0, p, 0, par][~flag_ind] = median_filter(par_copy, size=window_size, mode="reflect")
     
+
         delay_and_tec_params_to_gains(
             params,
             gains,
